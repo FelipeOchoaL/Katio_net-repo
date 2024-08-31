5586dd39e3898c4203c6a9cb9729bc2de11ee66f
FelipeOchoaL <pipeochoa22@gmail.com>
2024-08-30 20:38:30 -0500

    Book Service

--- katio-net.Business/IServices/IBookService.cs
+++ katio-net.Business/IServices/IBookService.cs
@@ -13,6 +13,8 @@ public interface IBookService
     public Task<BaseMessage<Books>>GetById(int id);//ya
     //Obtener por el titulo
     public Task<BaseMessage<Books>> GetByName(String Title);//ya
+    public Task<BaseMessage<Books>> GetByAuthor(int AuthorId);
+    public Task<BaseMessage<Books>> GetByAuthor(string name);
     //Update
     public Task<BaseMessage<Books>> Update(int Id, Books book);
     Task<BaseMessage<Books>> CreateBook(Books books);   
